a5c7b3a954ca39324892a75576dc5b04b8ffe60e
ValentinoVizner <valentino.vizner@gmail.com>
2020-03-17 19:26:13 +0100

    [INIT]: Basics of Rust

--- src/functions.rs
+++ src/functions.rs
@@ -1,1 +1,9 @@
-pub fn run() {}
+// Functions - Used t ostore bloc of code for re-use
+
+pub fn run() {
+    greeting("Hi", "Marija");
+}
+
+fn greeting(greet: &str, name: &str) {
+    println!("{} {}, nice to meet you!", greet, name);
+}
